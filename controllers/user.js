f27afe897f323e4d8c882c0d966aedcad8a64b1f
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-02-09 14:43:32 +0500

    Add password encryption using bcrypt

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const userSchema = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
 
 exports.signUp = async (req, res) => {
@@ -29,7 +30,8 @@ exports.signIn = async (req, res) => {
         if (!user) {
             return res.status(400).json({ message: "User not found" })
         }
-        if (password !== user.password) {
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
             return res.status(400).json({ message: "Invalid credentials" })
         }
         res.status(200).json({ message: "User signed in successfully" })
--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -14,5 +15,13 @@ const userSchema = new mongoose.Schema({
         required: true,
     }
 }, { timestamps: true })
+
+userSchema.pre('save', async function(next) {
+    if (this.isModified('password')) {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+    }
+    next();
+});
 
 module.exports = mongoose.model('User', userSchema)
