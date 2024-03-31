e4deeadde92a75b51ccea6c4b70693f1217050d8
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-03-31 05:57:59 +0500

    Add database schema for Xpense application

---
+++ schema.sql
@@ -0,0 +1,46 @@
+use Xpense;
+
+CREATE TABLE Category
+(
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255) DEFAULT NULL
+);
+
+CREATE TABLE Users
+(
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE Income
+(
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    user_id INT FOREIGN KEY REFERENCES Users(id),
+    title VARCHAR(50) NOT NULL,
+    amount DECIMAL(20, 2) NOT NULL,
+    type VARCHAR(255) DEFAULT 'income',
+    date DATE NOT NULL,
+    category_id INT FOREIGN KEY REFERENCES Category(id),
+    description VARCHAR(255) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE Expense
+(
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    user_id INT FOREIGN KEY REFERENCES Users(id),
+    title VARCHAR(50) NOT NULL,
+    amount DECIMAL(20, 2) NOT NULL,
+    type VARCHAR(255) DEFAULT 'Expense',
+    date DATE NOT NULL,
+    category_id INT FOREIGN KEY REFERENCES Category(id),
+    description VARCHAR(255) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
