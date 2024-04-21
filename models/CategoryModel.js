670eca9ca7f4df0571aa4ca14786de0532914d6a
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-04-21 13:51:21 +0500

    Add Category model

---
+++ models/CategoryModel.js
@@ -0,0 +1,20 @@
+const { DataTypes } = require('sequelize');
+const {sequelize} = require('../db/db.js');
+const User = require('./userModel.js');
+
+const Category = sequelize.define('Category', {
+    id:{
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    type:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    name:{
+        type: DataTypes.STRING,
+        allowNull: false
+    }});
+Category.sync();
+module.exports = Category;
