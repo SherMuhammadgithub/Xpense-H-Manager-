dd0a4182c6e7749393eb83265423d6327b48ab12
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-04-21 17:50:54 +0500

    Add api endpoints in transaction.js

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,21 +1,41 @@
-const { addExpense, getExpense, deleteExpense } = require('../controllers/expense');
-const { addIncome, getIncome, deleteIncome } = require('../controllers/income');
-const { signUp, signIn , changePass} = require('../controllers/user');
+const { addExpense, getExpense, deleteExpense, updateExpense } = require('../controllers/expense');
+const { addIncome, getIncome, deleteIncome, updateIncome} = require('../controllers/income');
+const { signUp, signIn , changePass, updateUser} = require('../controllers/user');
+const {getCategory, addCategory, deleteCategory} = require('../controllers/category');
+const {getGoal,addGoal,deleteGoal,updateGoal} = require('../controllers/goals');
+const {getNotification,addNotification,deleteNotification,seenNotification} = require('../controllers/notification');
 
 const router = require('express').Router();
 
 
-
+// Income
 router.post('/add-income', addIncome)
     .get('/get-income/:userId', getIncome)
     .delete('/delete-income/:id', deleteIncome)
+    .post('/update-income', updateIncome)
+    // Expense
     .post('/add-expense', addExpense)
     .get('/get-expense/:userId', getExpense)
     .delete('/delete-expense/:id', deleteExpense)
+    .post('/update-expense', updateExpense)
+    // User
     .post('/signUp', signUp)
     .post('/signIn', signIn)
     .post('/changePass', changePass)
-
-
+    .post('/updateUser', updateUser)
+    // Category
+    .get('/get-category', getCategory)
+    .post('/add-category', addCategory)
+    .delete('/delete-category/:id', deleteCategory)
+    // Goal
+    .get('/get-goal/:userId', getGoal)
+    .post('/add-goal', addGoal)
+    .delete('/delete-goal/:id', deleteGoal)
+    .post('/update-goal', updateGoal)
+    // Notification
+    .get('/get-notification/:userId', getNotification)
+    .post('/add-notification', addNotification)
+    .delete('/delete-notification/:id', deleteNotification)
+    .post('/seen-notification/:id', seenNotification);
 
 module.exports = router;
