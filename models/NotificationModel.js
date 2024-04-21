d2580bba9cbee1b8cb300b1f023ef10909007fd3
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-04-21 14:03:58 +0500

    Add notification model

---
+++ models/NotificationModel.js
@@ -0,0 +1,32 @@
+const {DataTypes} = require('sequelize');
+const {sequelize} = require('../db/db.js');
+const {User} = require('./userModel.js');
+
+const Notification = sequelize.define('Notification', {
+    id:{
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    user_id:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: User,
+            key: 'id'
+        }
+    },
+    message:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    timeStamps:{
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    Seen:{
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    }});
+Notification.sync();
+module.exports = Notification;
