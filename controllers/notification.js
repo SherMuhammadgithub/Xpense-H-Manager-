83ea2b42356d90804965ac111cdd8e36a06c0476
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-04-21 17:43:47 +0500

    Add notification controller

---
+++ controllers/notification.js
@@ -0,0 +1,55 @@
+const {Notification} = require('../models/NotificationModel')
+
+exports.addNotification = async(req,res) => {
+    const { user_id, message } = req.body;
+    try {
+        if (!user_id || !message) {
+            return res.status(400).json({ message: "All fields are required" })
+        }
+        const notification = await Notification.create({
+            user_id,
+            message
+        });
+        res.status(200).json({ message: "Notification added successfully" })
+    } catch (error) {
+        res.status(500).json({ message: "Server Error" })
+    }
+}
+
+exports.getNotification = async(req,res) => {
+    try {
+        const userId = req.params.userId;
+        const notification = await Notification.findAll({ where: { user_id: userId } });
+        res.status(200).json(notification);
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" })
+    }
+}
+
+exports.deleteNotification = async(req,res) => {
+    const { id } = req.params;
+    try {
+        const notification = await Notification.destroy({ where: { id } });
+        if (notification) {
+            res.status(200).json({ message: "Notification deleted successfully" });
+        } else {
+            res.status(400).json({ message: "Notification not found" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" })
+    }
+}
+
+exports.seenNotification = async(req,res) => {
+    const { id } = req.params;
+    try {
+        const notification = await Notification.update({ seen: true }, { where: { id } });
+        if (notification) {
+            res.status(200).json({ message: "Notification seen successfully" });
+        } else {
+            res.status(400).json({ message: "Notification not found" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" })
+    }
+}
