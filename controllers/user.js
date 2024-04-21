978d4ae6e0ee06e37bfbee10035cc88de2f88d96
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-04-21 19:45:07 +0500

    Fix delete User function

--- controllers/user.js
+++ controllers/user.js
@@ -103,13 +103,13 @@ exports.deleteUser = async (req, res) => {
     const { id } = req.params;
 
     try {
-        const user = await User.destroy({ id });
+        const user = await User.destroy({ where: { id: id } });
         if (user) {
             res.status(200).json({ message: "User deleted successfully" });
         } else {
             res.status(400).json({ message: "User not found" });
         }
     } catch (error) {
-        res.status(500).json({ message: "Internal server error" })
+        res.status(500).json({ message: "Internal server error", error: error.message})
     }
 }
