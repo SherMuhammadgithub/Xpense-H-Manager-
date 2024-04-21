568d7aac5634011e5dbdbceb3ebbf2286ae24ad9
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-04-21 17:14:33 +0500

    Add update income

--- controllers/income.js
+++ controllers/income.js
@@ -58,7 +58,35 @@ exports.getIncome = async (req, res) => {
         res.status(500).json({ message: "Internal server error" })
     }
 }
+exports.updateIncome = async(req,res) => {
+    const { title, amount, category, description, date, user_id } = req.body;
+    const incomeId = req.params.id;
+    try {
+        if (!title || !amount || !category || !description || !date || !user_id) {
+            return res.status(400).json({ message: "All fields are required" })
+        }
+        if (amount < 0 || typeof amount !== 'number') {
+            return res.status(400).json({ message: "Amount cannot be negative" })
+        }
+        const categoryData = await Category.findOne({ where: { name: category }, attributes: ['id']});
+        if (!categoryData) {
+            return res.status(400).json({ message: "Category does not exist" })
+        }
+        const category_id = categoryData.get('id');
+        const income = await Income.update({
+            user_id,
+            title,
+            amount,
+            category_id,
+            description,
+            date
+        }, { where: { id: incomeId } });
 
+        res.status(200).json({ message: "Income updated successfully" })
+    } catch (error) {
+        res.status(500).json({ message: "Server Error" })
+    }
+}    
 exports.deleteIncome = async (req, res) => {
     try {
         const incomeId = req.params.id;
