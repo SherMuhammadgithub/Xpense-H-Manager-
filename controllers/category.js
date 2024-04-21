31285558a65b0805a0c4578052c819fc01eabf2b
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-04-21 14:28:29 +0500

    Add category controller with addCategory, getCategory, and deleteCategory methods

---
+++ controllers/category.js
@@ -0,0 +1,39 @@
+
+exports.addCategory = async (req, res) => {
+    const {type, name} = req.body;
+    try {
+        if (!type || !name) {
+            return res.status(400).json({ message: "All fields are required" })
+        }
+        const category = await Category.create({
+            type,
+            name
+        });
+        res.status(200).json({ message: "Category added successfully" })
+    } catch (error) {
+        res.status(500).json({ message: "Server Error" })
+    }
+}
+
+exports.getCategory = async (req, res) => {
+    try {
+        const category = await Category.findAll();
+        res.status(200).json(category);
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" })
+    }
+}
+
+exports.deleteCategory = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const category = await Category.destroy({ where: { id } });
+        if (category) {
+            res.status(200).json({ message: "Category deleted successfully" });
+        } else {
+            res.status(400).json({ message: "Category not found" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" })
+    }
+}
