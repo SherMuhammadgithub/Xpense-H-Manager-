b874e47625fb9305887b1201e1644c7a12e71754
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-04-21 14:01:00 +0500

    Add GoalsModel

---
+++ models/GoalsModel.js
@@ -0,0 +1,28 @@
+const { DataTypes } = require('sequelize');
+const {sequelize} = require('../db/db.js');
+const {Category} = require('./CategoryModel.js');
+
+const Goals = sequelize.define('Goals', {
+    id:{
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    category_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: Category,
+            key: 'id'
+        }
+    },
+    Amount:{
+        type: DataTypes.DECIMAL(20, 2),
+        allowNull: false
+    },
+    description:{
+        type: DataTypes.STRING,
+        allowNull: false
+    }});
+Goals.sync();
+module.exports = Goals;
