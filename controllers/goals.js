87a59ac1c6dacadc5f030fd214d26d80d119e82e
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-04-22 13:55:24 +0500

    update UpdateGoal method in goals controller

--- controllers/goals.js
+++ controllers/goals.js
@@ -1,69 +1,76 @@
-const Goals = require('../models/GoalsModel');
+const Goals = require("../models/GoalsModel");
 
 exports.addGoal = async (req, res) => {
-    const {category_id,user_id, amount ,description, title} = req.body;
-    try {
-        if (!category_id || !user_id || !amount || !description,!title) {
-            return res.status(400).json({ message: "All fields are required" })
-        }
-        const goal = await Goals.create({
-            category_id,
-            user_id,
-            title,
-            amount,
-            description
-        });
-        res.status(200).json({ message: "Goal added successfully" })
-    } catch (error) {
-        res.status(500).json({ message: "Server Error"})
-    }}
+  const { category_id, user_id, amount, description, title } = req.body;
+  try {
+    if ((!category_id || !user_id || !amount || !description, !title)) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    const goal = await Goals.create({
+      category_id,
+      user_id,
+      title,
+      amount,
+      description,
+    });
+    res.status(200).json({ message: "Goal added successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
+};
 
 exports.getGoal = async (req, res) => {
-    try {
-        const user_id = req.params.user_id;
-        
-        let query = { user_id: user_id };
-        let sortOptions = [['createdAt', 'DESC']];
-        const goal = await Goals.findAll({ where: query, order: sortOptions });
-        res.status(200).json(goal);
-    }
-    catch (error) {
-        res.status(500).json({ message: "Internal server error" })
-    }
-}
+  try {
+    const user_id = req.params.user_id;
 
-exports.updateGoal = async(req,res) => {
-    const {category_id, user_id, amount ,description, title, id} = req.body;
-    try {
-        if (!category_id || !amount || !user_id || !description,!title) {
-            return res.status(400).json({ message: "All fields are required" })
-        }
-        if (amount < 0 || typeof amount !== 'number') {
-            return res.status(400).json({ message: "Amount cannot be negative" })
-        }
-        const goal = await Goals.update({
-            category_id,
-            user_id,
-            title,
-            amount,
-            description
-        }, { where: { id } });
-        res.status(200).json({ message: "Goal updated successfully" })
-    } catch (error) {
-        res.status(500).json({ message: "Server Error" })
+    let query = { user_id: user_id };
+    let sortOptions = [["createdAt", "DESC"]];
+    const goal = await Goals.findAll({ where: query, order: sortOptions });
+    res.status(200).json(goal);
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+exports.updateGoal = async (req, res) => {
+  const { category_id, user_id, amount, description, title, id } = req.body;
+  try {
+    if ((!category_id || !amount || !user_id || !description, !title)) {
+      return res.status(400).json({ message: "All fields are required" });
     }
-}
+    if (amount < 0 || typeof amount !== "number") {
+      return res.status(400).json({ message: "Amount cannot be negative" });
+    }
+    const [affectedRows] = await Goals.update(
+      {
+        category_id,
+        user_id,
+        title,
+        amount,
+        description,
+      },
+      { where: { id } }
+    );
+    if (affectedRows === 0) {
+      return res.status(400).json({ message: "Goal not found" });
+    } else {
+      res.status(200).json({ message: "Goal updated successfully" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
+};
 
 exports.deleteGoal = async (req, res) => {
-    const { id } = req.params;
-    try {
-        const goal = await Goals.destroy({ where: { id } });
-        if (goal) {
-            res.status(200).json({ message: "Goal deleted successfully" });
-        } else {
-            res.status(400).json({ message: "Goal not found" });
-        }
-    } catch (error) {
-        res.status(500).json({ message: "Internal server error" })
+  const { id } = req.params;
+  try {
+    const goal = await Goals.destroy({ where: { id } });
+    if (goal) {
+      res.status(200).json({ message: "Goal deleted successfully" });
+    } else {
+      res.status(400).json({ message: "Goal not found" });
     }
-}
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
