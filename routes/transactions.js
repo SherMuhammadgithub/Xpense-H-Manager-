d017147a945d1b06d571c72bb0ac29bbfc801cce
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-04-26 11:05:28 +0500

    Refactor routes/transactions.js for consistency and readability

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,44 +1,74 @@
-const { addExpense, getExpense, deleteExpense, updateExpense } = require('../controllers/expense');
-const { addIncome, getIncome, deleteIncome, updateIncome} = require('../controllers/income');
-const { signUp, signIn , changePass, updateUser, deleteUser, getUser} = require('../controllers/user');
-const {getCategory, addCategory, deleteCategory} = require('../controllers/category');
-const {getGoal,addGoal,deleteGoal,updateGoal} = require('../controllers/goals');
-const {getNotification,addNotification,deleteNotification,seenNotification} = require('../controllers/notification');
+const {
+  addExpense,
+  getExpense,
+  deleteExpense,
+  updateExpense,
+} = require("../controllers/expense");
+const {
+  addIncome,
+  getIncome,
+  deleteIncome,
+  updateIncome,
+} = require("../controllers/income");
+const {
+  signUp,
+  signIn,
+  changePass,
+  updateUser,
+  deleteUser,
+  getUser,
+} = require("../controllers/user");
+const {
+  getCategory,
+  addCategory,
+  deleteCategory,
+} = require("../controllers/category");
+const {
+  getGoal,
+  addGoal,
+  deleteGoal,
+  updateGoal,
+} = require("../controllers/goals");
+const {
+  getNotification,
+  addNotification,
+  deleteNotification,
+  seenNotification,
+} = require("../controllers/notification");
 
-const router = require('express').Router();
-
+const router = require("express").Router();
 
 // Income
-router.post('/add-income', addIncome)
-    .get('/get-income/:userId', getIncome)
-    .delete('/delete-income/:id', deleteIncome)
-    .put('/update-income', updateIncome)
-    // Expense
-    .post('/add-expense', addExpense)
-    .get('/get-expense/:userId', getExpense)
-    .delete('/delete-expense/:id', deleteExpense)
-    .put('/update-expense', updateExpense)
-    // User
-    .post('/signUp', signUp)
-    .post('/signIn', signIn)
-    .post('/changePass', changePass)
-    .put('/update-user', updateUser)
-    .delete('/delete-user/:id', deleteUser)
-    .get('/get-users', getUser)
-    // Category
-
-    .get('/get-category', getCategory)
-    .post('/add-category', addCategory)
-    .delete('/delete-category/:id', deleteCategory)
-    // Goal
-    .get('/get-goal/:user_id', getGoal)
-    .post('/add-goal', addGoal)
-    .delete('/delete-goal/:id', deleteGoal)
-    .put('/update-goal', updateGoal)
-    // Notification
-    .get('/get-notification/:userId', getNotification)
-    .post('/add-notification', addNotification)
-    .delete('/delete-notification/:id', deleteNotification)
-    .put('/seen-notification/:id', seenNotification);
+router
+  .post("/add-income", addIncome)
+  .get("/get-income/:userId", getIncome)
+  .delete("/delete-income/:id", deleteIncome)
+  .put("/update-income", updateIncome)
+  // Expense
+  .post("/add-expense", addExpense)
+  .get("/get-expense/:userId", getExpense)
+  .delete("/delete-expense/:id", deleteExpense)
+  .put("/update-expense", updateExpense)
+  // User
+  .post("/signUp", signUp)
+  .post("/signIn", signIn)
+  .post("/changePass", changePass)
+  .put("/update-user", updateUser)
+  .delete("/delete-user/:id", deleteUser)
+  .get("/get-users", getUser)
+  // Category
+  .get("/get-category", getCategory)
+  .post("/add-category", addCategory)
+  .delete("/delete-category/:id", deleteCategory)
+  // Goal
+  .get("/get-goal/:user_id", getGoal)
+  .post("/add-goal", addGoal)
+  .delete("/delete-goal/:id", deleteGoal)
+  .put("/update-goal", updateGoal)
+  // Notification
+  .get("/get-notification/:userId", getNotification)
+  .post("/add-notification", addNotification)
+  .delete("/delete-notification/:id", deleteNotification)
+  .put("/seen-notification/:id", seenNotification);
 
 module.exports = router;
