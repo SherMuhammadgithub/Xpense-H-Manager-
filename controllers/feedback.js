e039125634f42cb435763e4f7ea8485d9b28293a
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-04-26 11:21:52 +0500

    Add feedback controller and route

---
+++ controllers/feedback.js
@@ -0,0 +1,21 @@
+const { Feedback } = require("../models/FeedbackModel.js");
+
+exports.addFeedback = async (req, res) => {
+  const { user_id, mood, rating, checkbox, message } = req.body;
+  if (!user_id || !mood || !rating || !checkbox) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
+  try {
+    const feedback = await Feedback.create({
+      user_id,
+      mood,
+      rating,
+      checkbox,
+      message,
+    });
+    res.status(200).json({ message: "Feedback added successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+    console.log(error);
+  }
+};
