d9c2f2b51d19eea79f57bf6fbbdbe809787dc965
M Ahmed Butt <144407474+Ahmed8881@users.noreply.github.com>
2024-05-02 22:49:24 +0500

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,18 +1,1 @@
-const { Sequelize } = require('sequelize');
 
-
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: './db/db.sqlite'
-});
-
-const db = async() =>{
-    try{
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-}
-// sequelize.sync();
-module.exports = {db, sequelize};
