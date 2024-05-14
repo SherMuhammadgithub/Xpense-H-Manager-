0f41a99cd2a176f965fe2170ea2084994c9b98b5
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-05-14 13:23:37 +0500

    Remove cors url

--- Frontend/src/Dashboard/GlobalData/globalContext.jsx
+++ Frontend/src/Dashboard/GlobalData/globalContext.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { createContext, useContext, useEffect, useState } from "react";
 
+
 const BASE_URL = `http://localhost:8080/api/v1/`;
 const GlobalContext = createContext();
 
